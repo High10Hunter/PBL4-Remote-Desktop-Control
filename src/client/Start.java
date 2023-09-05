91f76ecb7745320fa16497fdb9a1d89a9698d021
Tran Dai Quy <trandaiquy2903@gmail.com>
2023-09-05 22:16:16 +0700

    initial commit

---
+++ src/client/Start.java
@@ -0,0 +1,27 @@
+package client;
+
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
+
+public class Start {
+    static String port = "4907";
+
+    public static void main(String[] args) {
+        String ip = JOptionPane.showInputDialog("Please enter server IP:");
+        new Start().initialize(ip, Integer.parseInt(port));
+    }
+
+    public void initialize(String ip, int port) {
+        try {
+            Socket sc = new Socket(ip, port);
+            System.out.println("Connected to server");
+            Authentication frame1 = new Authentication(sc);
+            frame1.setSize(300, 80);
+            frame1.setLocation(500, 300);
+            frame1.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
